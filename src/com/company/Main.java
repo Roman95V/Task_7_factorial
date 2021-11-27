522d6c0aace94f923c0962fd633f5fb3442da8b1
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-11-27 18:04:26 +0200

    new commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int number = 10;
+    int factorial = 1;
+    for (int i=1; i<=number; i++){
+        factorial = factorial*i;
+    }
+    System.out.println("Factorial with: " + number + "! = " + factorial);
+    }
+}
